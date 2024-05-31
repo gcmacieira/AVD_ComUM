33a0b29f8f4f13b86cd4e362f4718add6afc2bb2
MariaBarros07 <146782401+MariaBarros07@users.noreply.github.com>
2024-05-31 01:42:50 +0100

    Delete Parte1_Códigos directory

--- Parte1_Códigos/get.artigos.py
+++
@@ -1,5 +0,0 @@
-from jjcli import *
-
-for n in range(13, 17):
-    comando = f'wget --no-check-certificate -r -c -l 2 "https://www.comumonline.com/etiqueta/universidade-do-minho/"'
-    qxsystem(comando)
